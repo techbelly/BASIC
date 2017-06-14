140d41db2a65b29d272bb154f5ac182b9bb13f81
Ben Griffiths <bengriffiths@gmail.com>
2017-06-14 07:34:26 +0100

    WIP

--- test/coverage_test.rb
+++ test/coverage_test.rb
@@ -40,31 +40,59 @@ paths = coverage.keys.select { |path| path =~ /BASIC/ }
 
 class CoverageCleaner < Struct.new(:coverage)
 
-  def process(node)
+  EMPTY = Object.new()
 
-    return node unless node.is_a? Parser::AST::Node
-    return node unless node.location.expression
+  def empty_node(c)
+    c == EMPTY || (c.is_a?(Parser::AST::Node) && c.type == :begin && c.children.empty?)
+  end
 
-    first_line, last_line = node.location.first_line-1, node.location.last_line-1
-    if coverage[first_line..last_line].compact.empty? or coverage[first_line..last_line].compact.inject(0, :+) == 0
-      :empty
-    else
-      node.updated(node.type, cleaned(node), {location: node.location})
+  def sole_branch(children)
+    non_empty = children.select {|c| !empty_node(c) }
+    if non_empty.length == 2
+      return non_empty[1]
     end
   end
 
+  def coverage_count(node)
+    first_line, last_line = node.location.first_line-1, node.location.last_line
+    coverage[first_line..last_line].compact.max || 0
+  end
+
+  def process(node)
+    return node unless node.is_a? Parser::AST::Node
+    return node unless node.location.expression
+    puts node.parent
+    if node.type != :begin && coverage_count(node) == 0
+      print "SKIPPING", node
+      return EMPTY
+    end
+
+    children = cleaned(node)
+
+    return EMPTY if node.type == :def && empty_node(children[2])
+
+    # if node.type == :if && sole_branch(children)
+    #   branch = sole_branch(children)
+    #   return branch if coverage_count(branch) == coverage_count(node)
+    # end
+
+    node.updated(node.type, children, {location: node.location})
+  end
+
   def cleaned(nodes)
-    nodes.to_a.map { |node| process(node) }.delete_if {|e| e == :empty }
+    nodes.to_a.map { |node| process(node) }.delete_if {|e| e == EMPTY }
   end
 end
 
-paths.each do | path |
-#path = paths[2]
+#paths.each do | path |
+path = paths[6]
   ruby_source = File.read(path)
   coverage_result = coverage[path]
-  coverage_result
+  puts "coverage", coverage_result.map.with_index{ |e, i| "#{i+1} #{e}"}
   parse_tree = Parser::CurrentRuby.parse(ruby_source)
+  puts "parse_tree", parse_tree
   cleaner = CoverageCleaner.new(coverage_result)
   cleaned = cleaner.process(parse_tree)
+  puts "cleaned_tree", cleaned
   puts Unparser.unparse(cleaned)
-end
+#end
